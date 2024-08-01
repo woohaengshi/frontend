1657e8d6ebd8ed9bd41e5bb53aae4785019d8572
Kong-E <kng001016@dgu.ac.kr>
2024-08-01 17:58:23 +0900

    design: 타이머 css

--- app/study/page.tsx
+++ app/study/page.tsx
@@ -1,5 +1,17 @@
+import Timer from '@/components/study/Timer';
+import { Box, Button, Container, Text } from '@radix-ui/themes';
+import styles from './page.module.css';
+
 export default function Study() {
   return (
-    <div></div>
+    <Container size="3" className={styles.container} height="100%">
+      <Text as="p" className={styles.text} size={'5'} weight={'medium'} align={'center'}>
+        다음 레벨업까지
+        <br />
+        02:59:47
+      </Text>
+      <Timer maxTime={10} />
+      <Button className={styles.floatingButton}></Button>
+    </Container>
   );
 }
