fa460ae2a146f999f8665b5058dcf8b925eadc28
Kong-E <kng001016@dgu.ac.kr>
2024-08-07 16:19:13 +0900

    [refactor/20] 로그인 페이지 리팩토링

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -5,42 +5,40 @@ import Link from 'next/link';
 export default function Login() {
   return (
     <Container size="2" p="6">
-      <Box>
-        <div className={styles.member_wrap}>
-          <header className={styles.title}>
-            <h2>로그인</h2>
-          </header>
-          <section className={styles.content}>
-            <form action="">
-              <div className={styles.input_box}>
-                <div className={styles.row}>
-                  <label htmlFor="user_name">이름</label>
-                  <div className={styles.input}>
-                    <input type="text" id="user_name" placeholder="이름을 입력해주세요." />
-                  </div>
-                </div>
-                <div className={styles.row}>
-                  <label htmlFor="user_pw">비밀번호</label>
-                  <div className={styles.input}>
-                    <input type="password" id="user_pw" placeholder="비밀번호를 입력해주세요." />
-                  </div>
+      <div className={styles.member_wrap}>
+        <header className={styles.title}>
+          <h2>로그인</h2>
+        </header>
+        <section className={styles.content}>
+          <form action="">
+            <div className={styles.input_box}>
+              <div className={styles.row}>
+                <label htmlFor="user_name">이름</label>
+                <div className={styles.input}>
+                  <input type="text" id="user_name" placeholder="이름을 입력해주세요." />
                 </div>
               </div>
-              <div className={styles.btn_box}>
-                <div id="loginBtn" className={styles.button}>
-                  <button type="submit">로그인</button>
+              <div className={styles.row}>
+                <label htmlFor="user_pw">비밀번호</label>
+                <div className={styles.input}>
+                  <input type="password" id="user_pw" placeholder="비밀번호를 입력해주세요." />
                 </div>
-              </div>
-            </form>
-            <div className={`${styles.btn_box} ${styles.btn_join}`}>
-              <p>아직 회원이 아니신가요?</p>
-              <div className={styles.button}>
-                <Link href={'/join'}>회원가입</Link>
               </div>
             </div>
-          </section>
-        </div>
-      </Box>
+            <div className={styles.btn_box}>
+              <div id="loginBtn" className={styles.button}>
+                <button type="submit">로그인</button>
+              </div>
+            </div>
+          </form>
+          <div className={`${styles.btn_box} ${styles.btn_join}`}>
+            <p>아직 회원이 아니신가요?</p>
+            <div className={styles.button}>
+              <Link href={'/join'}>회원가입</Link>
+            </div>
+          </div>
+        </section>
+      </div>
     </Container>
   );
 }
