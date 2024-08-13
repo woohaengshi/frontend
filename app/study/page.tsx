2c5ac0958d56722c447219d5f61253ff3bc8e3dc
Kong-E <kng001016@dgu.ac.kr>
2024-08-14 00:59:23 +0900

    [feat/#53]: API 호출 테스트

--- app/study/page.tsx
+++ app/study/page.tsx
@@ -1,24 +1,11 @@
-'use client';
-
-import { useState } from 'react';
-import SubjectEditForm from '../../components/study/SubjectEditForm';
-import stylesEdit from '../../components/study/SubjectEditForm.module.css';
 import Timer from '@/components/study/Timer';
 import { Container } from '@radix-ui/themes';
 import styles from './page.module.css';
 import SubjectSelectButton from '@/components/study/SubjectSelectButton';
 import { getTimer } from '@/api/study';
 
-export default function Study() {
-  const [showForm, setShowForm] = useState(false);
-
-  const handleButtonClick = () => {
-    setShowForm(!showForm);
-  };
-
-  const closeSubjectEditForm = () => {
-    setShowForm(false);
-  };
+export default async function Study() {
+  const timerResponse = await getTimer();
 
   return (
     <Container size="3" className={styles.container} height="100%">
