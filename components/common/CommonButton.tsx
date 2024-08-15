d1b6241585d617f89c7b1921482b79823c331bdf
Kong-E <kng001016@dgu.ac.kr>
2024-08-15 20:28:16 +0900

    [refactor/#59]: CommonButton interface 추가

--- components/common/CommonButton.tsx
+++ components/common/CommonButton.tsx
@@ -3,7 +3,14 @@
 import styles from './CommonButton.module.css';
 import Link from 'next/link';
 
-export default function CommonButton({ type, href, style, children }) {
+interface CommonButtonProps {
+  type: 'submit' | 'button' | 'link';
+  href?: string;
+  style: 'dark_purple' | 'light_purple' | 'dark_gray' | 'light_gray';
+  children: React.ReactNode;
+}
+
+export default function CommonButton({ type, href, style, children }: CommonButtonProps) {
   return (
     <>
       {type == 'submit' ? (
@@ -15,7 +22,7 @@ export default function CommonButton({ type, href, style, children }) {
           {children}
         </button>
       ) : (
-        <Link href={href} className={styles[style]}>
+        <Link href={href ? href : ''} className={styles[style]}>
           {children}
         </Link>
       )}
