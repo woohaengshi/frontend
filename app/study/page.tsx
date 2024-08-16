caf3a670226d0683898f851cf1ee2f5c3117fa36
Kong-E <kng001016@dgu.ac.kr>
2024-08-16 16:48:53 +0900

    [refactor/#59]: utils 디렉토리 생성 및 유틸 함수 분리

--- app/study/page.tsx
+++ app/study/page.tsx
@@ -2,7 +2,7 @@ import Timer from '@/components/study/Timer';
 import { Container } from '@radix-ui/themes';
 import styles from './page.module.css';
 import SubjectSelectButton from '@/components/study/SubjectSelectButton';
-import { getTimer, postTimer } from '@/api/study';
+import { getTimer } from '@/api/study';
 
 export default async function Study() {
   const timerResponse = await getTimer();
