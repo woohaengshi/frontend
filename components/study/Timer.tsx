6b6b793e5bfa2829b8b443623e505bf0351b2221
Kong-E <kng001016@dgu.ac.kr>
2024-08-16 09:30:10 +0900

    [feat/#59]: 타이머 date 수정

--- components/study/Timer.tsx
+++ components/study/Timer.tsx
@@ -14,7 +14,7 @@ const getCurrentDate = () => {
   const today = new Date();
   const year = today.getFullYear();
   const month = String(today.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1 필요
-  const day = String(today.getDate() - 1).padStart(2, '0');
+  const day = String(today.getDate() - (today.getHours() < 5 ? 1 : 0)).padStart(2, '0');
 
   return `${year}-${month}-${day}`;
 };
