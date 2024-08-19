bad46bea51c10ea670a5be76f2584ec9ecf41bba
riverkite0708 <0908xmrqkr@gmail.com>
2024-08-19 13:46:15 +0900

    [style/#56]: mypage 컴포넌트 eslint 적용

--- components/mypage/UserProfile.tsx
+++ components/mypage/UserProfile.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import { Avatar, Box, Strong, Text } from "@radix-ui/themes";
-import { useRef, useState } from "react";
+import { Avatar, Box, Strong, Text } from '@radix-ui/themes';
+import { useRef, useState } from 'react';
 import styles from './UserProfile.module.css';
 import ico_profile_img_file from '@/assets/icons/profile_img_file.png';
-import Image from "next/image";
-import MypageTabMenu from "./MypageTabMenu";
+import Image from 'next/image';
+import MypageTabMenu from './MypageTabMenu';
 
 export default function UserProfile() {
   const [imgUrl, setImgUrl] = useState('');
@@ -17,26 +17,32 @@ export default function UserProfile() {
     reader.readAsDataURL(file);
     reader.onloadend = () => {
       setImgUrl(reader.result);
-    }
+    };
   };
- 
+
   return (
     <section className="user_profile">
       <div className={styles.user_info}>
         <div className={styles.img_box}>
-          <div className={styles.back_img} style={{backgroundImage: `url(${imgUrl ? imgUrl : ''})`,}}>
+          <div className={styles.back_img} style={{ backgroundImage: `url(${imgUrl ? imgUrl : ''})` }}>
             <Avatar size="8" fallback="" radius="full"></Avatar>
           </div>
           <div className={styles.btn_file}>
-            <input type="file" ref={imgRef} onChange={onChangeImage}/>
-            <button onClick={()=>{imgRef.current.click()}}>
-              <Image src={ico_profile_img_file} alt="프로필 이미지 변경하기" width={18} height={18}/>
+            <input type="file" ref={imgRef} onChange={onChangeImage} />
+            <button
+              onClick={() => {
+                imgRef.current.click();
+              }}
+            >
+              <Image src={ico_profile_img_file} alt="프로필 이미지 변경하기" width={18} height={18} />
             </button>
           </div>
         </div>
         <Box mt="3" className={styles.txt_box}>
           <Strong>홍길동</Strong>
-          <Text as="p" size="3" weight="medium">클라우드 서비스</Text>
+          <Text as="p" size="3" weight="medium">
+            클라우드 서비스
+          </Text>
         </Box>
       </div>
       <MypageTabMenu />
