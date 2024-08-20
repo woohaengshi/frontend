ba9ba01af56150ee3e5a606544a1e592a6c61c63
riverkite0708 <0908xmrqkr@gmail.com>
2024-08-20 13:39:31 +0900

    [feat/#76]: 헤더 기본 이미지 설정

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import { Avatar, Box, Flex, Heading, Strong, Text } from '@radix-ui/themes';
 import styles from './Header.module.css';
 import HeaderNav from './HeaderNav';
+import rankingImg from '@/assets/icons/ranking_profile_img.png';
+import Image from 'next/image';
 
 export default function Header() {
   return (
@@ -19,14 +21,16 @@ export default function Header() {
           <div className={styles.user_info}>
             <Flex align="center" gap="2" asChild>
               <Link href="/mypage">
-                {/* <div
+                <div
                   className={styles.user_name}
                   style={{
-                    backgroundImage: `url('https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop')`,
+                    // 유저 프로필 이미지
+                    backgroundImage: `url('')`,
                   }}
                 >
-                  <Avatar fallback="A" />
-                </div> */}
+                  {/* 기본 이미지 */}
+                  <Image src={rankingImg.src} alt={`프로필 이미지`} width={40} height={40} />
+                </div>
                 <div className={styles.user_txt}>
                   <Strong>홍길동</Strong>
                   <Text as="p" size="2" mt="1" weight="medium">
