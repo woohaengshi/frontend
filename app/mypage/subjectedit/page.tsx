eb81c38b0f2ba92ba377717c0095a1f157bae915
doyi0107 <doyi9917@naver.com>
2024-08-22 00:38:53 +0900

    [fix/#58]:prettier 에러 수정

--- app/mypage/subjectedit/page.tsx
+++ app/mypage/subjectedit/page.tsx
@@ -4,12 +4,11 @@
 import SubjectEditForm from '@/components/study/SubjectEditForm';
 import { useSubjectStore } from '@/store/subjectStore';
 export default function Page() {
-
   const { setEditing, saveEditing } = useSubjectStore((state) => ({
     setEditing: state.setEditing,
     saveEditing: state.saveEditing,
   }));
-  
+
   const customStyle = {
     height: '400px',
   };
