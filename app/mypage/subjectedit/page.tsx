37e07d05b3074263ce7e691f6571f840913b12bd
doyi0107 <doyi9917@naver.com>
2024-08-21 12:33:09 +0900

    [fix/#58]:prettier 에러수정

--- app/mypage/subjectedit/page.tsx
+++ app/mypage/subjectedit/page.tsx
@@ -5,8 +5,8 @@ import SubjectEdit from '@/components/study/SubjectEditForm';
 import { useSubjectStore } from '@/store/subjectStore';
 export default function Page() {
   // Zustand 스토어에서 상태와 액션 가져오기
-  const { setEditing,saveEditing } = useSubjectStore((state) => ({
-    setEditing: state.setEditing, 
+  const { setEditing, saveEditing } = useSubjectStore((state) => ({
+    setEditing: state.setEditing,
     saveEditing: state.saveEditing,
   }));
   const customStyle = {
