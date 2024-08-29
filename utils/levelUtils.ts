d41af35e5a78032ecb53015e37ee1207f0f31571
riverkite0708 <0908xmrqkr@gmail.com>
2024-08-29 22:17:47 +0900

    [fix/레벨별 기록색깔 수정]

--- utils/levelUtils.ts
+++ utils/levelUtils.ts
@@ -1,8 +1,8 @@
 export const levelColor = (time: number): string => {
   const hours = Math.floor(time / 3600);
 
-  if (hours >= 3) return 'level3';
-  if (hours >= 2) return 'level2';
+  if (hours >= 6) return 'level3';
+  if (hours >= 3) return 'level2';
 
   return 'level1';
 };
