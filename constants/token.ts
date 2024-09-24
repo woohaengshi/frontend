80ad58fdb1810137924b22e78aa3e5f74cf7cf10
Kong-E <kng001016@dgu.ac.kr>
2024-09-24 09:12:54 +0900

    fix: 토큰 만료 기간 수정

--- constants/token.ts
+++ constants/token.ts
@@ -1,3 +1,3 @@
 // export const ACCESS_TOKEN_EXPIRES = 3 * 60 * 60 * 1000; // 3시간
-export const ACCESS_TOKEN_EXPIRES = 2505600; // 1달(29일)
+export const ACCESS_TOKEN_EXPIRES = 2505600 * 1000; // 1달(29일)
 // export const ACCESS_TOKEN_EXPIRES = 10 * 1000; // 10 seconds
